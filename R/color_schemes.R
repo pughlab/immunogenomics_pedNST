3474d0aec7168878faf41f89fbd22d522416b8a8
Arash Nabbi <nabbi.arash@gmail.com>
2021-10-04 15:00:38 -0400

    added notebooks and functions

---
+++ R/color_schemes.R
@@ -0,0 +1,29 @@
+group_col <- c("ICGC" = "#1B9E77",
+               "CBTN" = "#D95F02",
+               "TARGET" = "#7570B3",
+               "TCGA" = "#E7298A")
+
+cohort_col <- c("PDX" = "#878787", #PDX
+                "ETMR" = "#76afa9", #ped
+                "MNG" = "#a6cee3", 
+                "MB" =  "#1f78b4",
+                "SCHW" =  "#678ba5",
+                "ATRT" =  "#33a02c",
+                "EPN" = "#e31a1c",
+                "pedHGG" = "#fdbf6f",
+                "pedLGG" = "#8763ae",
+                "NFB" = "#cab2d6",
+                "CPH" = "#cccc7a",
+                "CP" = "#a9a9a9",
+                "NBL" = "#b2df8a",
+                
+                "SKCM" = "#7c9c60", #Adult
+                "LGG" = "#753a00",
+                "GBM" = "#6a3d9a",
+                "OV" = "#afaf78",
+                "PRAD" = "#a85426",
+                "LUAD" = "#ad3837",
+                "COAD" = "#726579",
+                
+                "EMPTY1" = "#FFFFFF", # some dummy colors
+                "EMPTY2" = "#FFFFFF")
