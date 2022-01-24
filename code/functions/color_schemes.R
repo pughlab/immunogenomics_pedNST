86eee63b6f1caa1b48bd0978e012df10834999c7
Arash Nabbi <nabbi.arash@gmail.com>
2022-01-24 13:40:42 -0500

    no message

--- code/functions/color_schemes.R
+++ code/functions/color_schemes.R
@@ -1,7 +1,8 @@
 group_col <- c("ICGC" = "#1B9E77",
                "CBTN" = "#D95F02",
                "TARGET" = "#7570B3",
-               "TCGA" = "#E7298A")
+               "TCGA" = "#E7298A",
+               "PDX (ITCC)" = "#878787")
 
 cohort_col <- c("PDX" = "#878787", #PDX
                 "ETMR" = "#76afa9", #ped
@@ -27,3 +28,40 @@ cohort_col <- c("PDX" = "#878787", #PDX
                 
                 "EMPTY1" = "#FFFFFF", # some dummy colors
                 "EMPTY2" = "#FFFFFF")
+
+cluster_col <- c("Pediatric inflamed" = "#d7191c", "Myeloid-driven" = "#fdae61",
+                 "Pediatric cold" = "#abd9e9", "Immune excluded" =  "#2c7bb6")
+  
+
+cri_col <- c("Wound Healing" = "#EF424C", "IFN-gamma dominant" = "#F9EF66", 
+             "Inflammatory" = "#81C341", "Lymphocyte depleted" = "#84D2E0", 
+             "Immunologically quiet" = "#4E5FAB", "TGFbeta dominant" = "#C36BAA")
+  
+gender_pal <- c("Male" = "#4dbf77",
+               "Female" = "#bf774d",
+               "Unknown" = "#878787")
+
+race_pal <- c("African" = "#00aedb",
+             "Asian" = "#8ec127",
+             "Other" = "#f47835",
+             "White" = "#a200ff",
+             "Unknown" = "#878787")
+
+age_pal <- c("Infants" = "#bd0026",
+            "Children" = "#fc4e2a",
+            "Adolescents" = "#feb24c",
+            "Young adults" = "#ffeda0",
+            "Adults" = "#80cdc1",
+            "Unknown" = "#878787")
+
+cogpal <- c("High Risk" = "#6a3d9a",
+            "Intermediate Risk" = "#a6cee3",
+            "Low Risk" = "#fb9a99")
+
+mycnpal <- c("Amplified" = "#e41a1c",
+             "Not Amplified" = "#377eb8",
+             "Unknown" = "light grey")
+
+
+
+
