7fe02454d2cf53028dfe82108f78dacc3daf247e
Arash Nabbi <nabbi.arash@gmail.com>
2022-07-22 13:29:12 -0400

    figure 5

--- code/Figure5.R
+++ code/Figure5.R
@@ -2,30 +2,32 @@
 # Figure 5
 ###############
 
-library(ggplot2)
-library(survival)
-library(survminer)
-
-# library(dplyr)
-# library(reshape2)
-# library(ggridges)
-# library(ggbeeswarm)
-# library(ggsignif)
-# library(cowplot)
-# library(gridExtra)
-# library(gtable)
-# library(grid)
-# library(ComplexHeatmap)
-# library(circlize)
-
+source("/code/functions/dependencies.R")
 source("/code/functions/ggplot2_theme.R")
 source("/code/functions/color_schemes.R")
 source("/code/functions/Heatmap_functions.R")
+source("/code/functions/Oncoprint_functions.R")
 source("/code/functions/plotting_functions.R")
 
 datapath <- "/data/"
+
 
 ###############
 # Figure 5A
 ###############
 
+
+
+
+###############
+# Compile in one file
+###############
+
+setwd("/results")
+
+plotflow:::mergePDF(
+  in.file = list.files(file.path("/results"), pattern = "Fig4_", full.names = TRUE),
+  file="Figure4.pdf"
+)
+
+do.call(file.remove, list(list.files("/results/", pattern = "Fig4_", full.names = TRUE)))
