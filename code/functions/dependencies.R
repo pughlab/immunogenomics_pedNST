74882d2d2592d41eac431bc0c9a8bb3549ffb48b
Arash Nabbi <nabbi.arash@gmail.com>
2022-07-26 11:33:16 -0400

    add broom

--- code/functions/dependencies.R
+++ code/functions/dependencies.R
@@ -4,6 +4,6 @@ mypackages <- c("dplyr", "reshape2", "tidyr",
                 "grid", "gridExtra", "gtable", "cowplot", 
                 "ComplexHeatmap", "circlize",
                 "survival", "survminer",
-                "plotflow", "qusage")
+                "plotflow", "qusage", "broom")
 
 suppressPackageStartupMessages(invisible(lapply(mypackages, library, character.only = TRUE)))
