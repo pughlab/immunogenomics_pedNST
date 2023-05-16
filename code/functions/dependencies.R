4f8c933f2b86c8d18b08865a9faf1605558eedc6
Arash Nabbi <nabbi.arash@gmail.com>
2023-05-16 12:51:49 -0400

    no message

--- code/functions/dependencies.R
+++ code/functions/dependencies.R
@@ -1,7 +1,7 @@
 
 mypackages <- c("dplyr", "reshape2", "tidyr",
                 "ggplot2", "ggridges", "ggbeeswarm", "ggsignif", "ggrepel",
-                "grid", "gridExtra", "gtable", "cowplot", 
+                "grid", "gridExtra", "gtable", "cowplot", "patchwork",
                 "ComplexHeatmap", "circlize",
                 "survival", "survminer", "egg",
                 "plotflow", "qusage", "broom")
